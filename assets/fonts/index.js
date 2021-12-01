19623639a89be6f8f962cec7fa5711295f8510b2
Drinnn <pedro_lontro@hotmail.com>
2021-12-01 11:04:39 -0300

    chore: add assets and constants

---
+++ assets/fonts/index.js
@@ -0,0 +1,22 @@
+const fonts = {
+  "roboto-black": require("./Roboto-Black.ttf"),
+  "roboto-blackItalic": require("./Roboto-BlackItalic.ttf"),
+  "roboto-bold": require("./Roboto-Bold.ttf"),
+  "roboto-boldItalic": require("./Roboto-BoldItalic.ttf"),
+  "roboto-italic": require("./Roboto-Italic.ttf"),
+  "roboto-light": require("./Roboto-Light.ttf"),
+  "roboto-lightItalic": require("./Roboto-LightItalic.ttf"),
+  "roboto-medium": require("./Roboto-Medium.ttf"),
+  "roboto-mediumItalic": require("./Roboto-MediumItalic.ttf"),
+  "roboto-regular": require("./Roboto-Regular.ttf"),
+  "roboto-thin": require("./Roboto-Thin.ttf"),
+  "roboto-thinItalic": require("./Roboto-ThinItalic.ttf"),
+  "robotoCondensed-bold": require("./RobotoCondensed-Bold.ttf"),
+  "robotoCondensed-boldItalic": require("./RobotoCondensed-BoldItalic.ttf"),
+  "robotoCondensed-italic": require("./RobotoCondensed-Italic.ttf"),
+  "robotoCondensed-light": require("./RobotoCondensed-Light.ttf"),
+  "robotoCondensed-lightItalic": require("./RobotoCondensed-LightItalic.ttf"),
+  "robotoCondensed-regular": require("./RobotoCondensed-Regular.ttf"),
+};
+
+export default fonts;
